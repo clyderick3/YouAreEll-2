8c042929348f20baaf92e97c3c30b773d9e75952
Clyde Broderick <clyderick3@gmail.com>
2022-04-03 22:09:40 -0400

    Better formatting for commented out section. For Each loop.

--- Client/src/main/java/controllers/MessageController.java
+++ Client/src/main/java/controllers/MessageController.java
@@ -24,44 +24,12 @@ public class MessageController {
         messagesSeen = objectMapper.readValue(messageString, new TypeReference<>(){});
         }
 
-//        ServerController serverController = ServerController.shared();
-//        JSONArray messagesJSON = serverController.messageGet();
-//
-//        for (int i = 0; i<messagesJSON.size(); i++) {
-//            Object object = messagesJSON.get(i);
-//            String string = object.toString();
-//            String[] messageList = string.split(",");
-//            String toID = messageList[0].substring(8);
-//            String sequence = messageList[1].substring(11);
-//            String message = messageList[2].substring(10);
-//            String fromID = messageList[3].substring(9);
-//            String timestamp = messageList[4].substring(12);
-//            Message messageObject = new Message(message,fromID,toID,timestamp,sequence);
-//            messagesSeen.add(messageObject);
-        }
-    }
-
-    private static MessageController myController = new MessageController();
-
-    public static MessageController shared() {
-        return myController;
-    }
-
     public ArrayList<Message> getMessages() {
-        Message[] array = messagesSeen.toArray(new Message[0]);
         ArrayList<Message> messages = new ArrayList<>();
-        for (int i =0; i<21; i++){
-            messages.add(array[i]);
+        for (Message i : messagesSeen) {
+            messages.add(i);
         }
-
-        ArrayList<Message> copy = (ArrayList<Message>) messages.stream()
-                .sorted(Comparator.comparing(Message::getTimestamp)).collect(Collectors.toList());
-        ArrayList<Message> mostRecent = new ArrayList<>();
-        for(int i = copy.size()-1; i > copy.size()-21; i--){
-            mostRecent.add(copy.get(i));
-        }
-        mostRecent.forEach(System.out::println);
-        return mostRecent;
+        return messages;
     }
 
     public ArrayList<Message> getMessagesForId(Id Id) {
@@ -77,5 +45,39 @@ public class MessageController {
     public Message postMessage(Id myId, Id toId, Message msg) {
         return null;
     }
- 
+
 }
+
+
+//        ServerController serverController = ServerController.shared();
+//        JSONArray messagesJSON = serverController.messageGet();
+//
+//        for (int i = 0; i<messagesJSON.size(); i++) {
+//            Object object = messagesJSON.get(i);
+//            String string = object.toString();
+//            String[] messageList = string.split(",");
+//            String toID = messageList[0].substring(8);
+//            String sequence = messageList[1].substring(11);
+//            String message = messageList[2].substring(10);
+//            String fromID = messageList[3].substring(9);
+//            String timestamp = messageList[4].substring(12);
+//            Message messageObject = new Message(message,fromID,toID,timestamp,sequence);
+//            messagesSeen.add(messageObject);
+//        }
+//    }
+//
+//    private static MessageController myController = new MessageController();
+//
+//    public static MessageController shared() {
+//        return myController;
+//    }
+//
+//        ArrayList<Message> copy = (ArrayList<Message>) messages.stream()
+//                .sorted(Comparator.comparing(Message::getTimestamp)).collect(Collectors.toList());
+//        ArrayList<Message> mostRecent = new ArrayList<>();
+//        for(int i = copy.size()-1; i > copy.size()-21; i--){
+//            mostRecent.add(copy.get(i));
+//        }
+//        mostRecent.forEach(System.out::println);
+//        return mostRecent;
+//    }
