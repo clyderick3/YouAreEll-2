64929de02510e4324d200119b4259bd520da34c3
Clyde Broderick <clyderick3@gmail.com>
2022-04-03 22:39:50 -0400

    Added constructor.

--- Client/src/main/java/youareell/YouAreEll.java
+++ Client/src/main/java/youareell/YouAreEll.java
@@ -10,31 +10,36 @@ public class YouAreEll {
         this.tt = t;
     }
 
+    public YouAreEll(MessageController messageController, IdController idController) {
+        this.tt = new TransactionController(messageController, idController);
+
+    }
+
     public static void main(String[] args) {
         // hmm: is this Dependency Injection?
 
-//        ServerController serverController = ServerController.shared();
-//        serverController.idGet();
-//        serverController.messageGet();
+        ServerController serverController = ServerController.shared();
+        serverController.idGet();
+        serverController.messageGet();
 
         MessageController messageController = MessageController.shared();
         System.out.println(messageController.messagesSeen);
 
-//        YouAreEll urlhandler = new YouAreEll(
-//            new TransactionController(
-//                new MessageController(), new IdController()
-//        ));
-//        System.out.println(urlhandler.MakeURLCall("/ids", "GET", ""));
-//        System.out.println(urlhandler.MakeURLCall("/messages", "GET", ""));
-//    }
-//
-//    public String get_ids() {
-//        return tt.makecall("/ids", "GET", "");
-//    }
-//
-//    public String get_messages() {
-//        return MakeURLCall("/messages", "GET", "");
-//    }
+        YouAreEll urlhandler = new YouAreEll(
+            new TransactionController(
+                new MessageController(), new IdController()
+        ));
+        System.out.println(urlhandler.MakeURLCall("/ids", "GET", ""));
+        System.out.println(urlhandler.MakeURLCall("/messages", "GET", ""));
+    }
+
+    public String get_ids() {
+        return tt.makecall("/ids", "GET", "");
+    }
+
+    public String get_messages() {
+        return MakeURLCall("/messages", "GET", "");
+    }
 
 
     }
